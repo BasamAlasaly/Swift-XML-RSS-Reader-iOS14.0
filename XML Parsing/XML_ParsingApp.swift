1c6ee56e881d80e1d49089bd3eb398cb6e8736ea
BasamAlasaly <bhasaly@gmail.com>
2021-08-07 01:40:36 -0700

    Add files via upload

---
+++ XML Parsing/XML_ParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  XML_ParsingApp.swift
+//  XML Parsing
+//
+//  Created by Basam Alasaly on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct XML_ParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
